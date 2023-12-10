5ae03da1f1b75d691dcda149164fc5cb312e3360
jgomezz <jgomezz@gmail.com>
2023-12-10 09:44:08 -0500

    First version

---
+++ src/main/java/pe/edu/tecsup/tienda/SpringTiendaWebCpeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTiendaWebCpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTiendaWebCpeApplication.class, args);
+	}
+
+}
